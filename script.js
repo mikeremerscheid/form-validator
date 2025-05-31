70dfe83fe69badab074f31f2d0d642774b8da7be
mikeremerscheid <mikeremerscheid@gmail.com>
2025-05-31 15:37:55 -0400

    added some basic js

--- script.js
+++ script.js
@@ -4,6 +4,40 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+} 
+
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success'
+}
+
+// Check email is valid
+function isValidEmail(email) {
+    const re = "^[w-.]+@([w-]+.)+[w-]{2,4}$";
+    return re.test(String(email).toLowerCase());
+}
+
+// Event Listener
 form.addEventListener('submit', function(e) {
-    console.log('submit');
+    e.preventDefault();
+
+    if (username.value === '') {
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Email is not valid')
+    } else showSuccess(email);
 });
